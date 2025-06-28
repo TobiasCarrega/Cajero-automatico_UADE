a97d481ecb659ce176de123306fcd37cc0815e71
tobillo <themastertob@gmail.com>
2025-06-28 19:50:42 -0300

    Agregar función de validación para el ingreso de DNI y simplificar el proceso de registro

--- cajero_automatico.py
+++ cajero_automatico.py
@@ -111,6 +111,24 @@ def agregarsuario(DNI,indice):
     indice = len(Lista_DNI)-1
     ordenamientoPorDNI()
 
+def validacion(informacion):
+    while informacion == "":
+        informacion= input("No puede estar vacío. Intente nuevamente: ")
+    else:
+        es_numero = True
+        i = 0
+        while i < len(informacion):
+            if informacion[i] < '0' or informacion[i] > '9':
+                es_numero = False
+            i = i + 1
+
+        if es_numero:
+            informacion = int(informacion)
+        else:
+            print("Debe ingresar solo números. Intente nuevamente: ")
+            informacion = ""  # fuerza a repetir
+    return informacion
+
 # Listas y contadores
 Lista_DNI = [43459748,46208734]
 Lista_nombre=["Tobias","lucia"]
@@ -133,33 +151,18 @@ while ejecucion:
         DNI = ""
         while DNI == "":
             DNI = input("Ingrese su número de documento: ")
-            
-            while DNI == "":
-                DNI= input("No puede estar vacío. Intente nuevamente: ")
-            else:
-                es_numero = True
-                i = 0
-                while i < len(DNI):
-                    if DNI[i] < '0' or DNI[i] > '9':
-                        es_numero = False
-                    i = i + 1
-
-                if es_numero:
-                    DNI = int(DNI)
-                else:
-                    print("Debe ingresar solo números. Intente nuevamente: ")
-                    DNI = ""  # fuerza a repetir
+            DNI=validacion(DNI)
 
         posicionBusqueda=busquedaEnLista(Lista_DNI,DNI)
         indice=posicionBusqueda
         if posicionBusqueda==-1:
-            print(indice)
             guardardni=int(input("Su numero de documento no se encuentra registrado ¿Quiere registrarse?(1=si 2=no)"))
             if guardardni==1:
                 agregarsuario(DNI,indice)
         else:
             print("bienvenido",Lista_nombre[indice] )
             print("=================================")
+            # print(Lista_PIN,Lista_nombre)
             verificacion = int(input("Ingrese su pin: "))
             if verificacion == Lista_PIN[indice]:
                 print("Clave correcta")
